f6809874bd505a58b5b09e293e621e07ad32c61a
DNKpp <DNKpp2011@gmail.com>
2020-05-06 21:28:30 +0200

    add Utility.hpp
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

---
+++ include/georithm/Utility.hpp
@@ -0,0 +1,56 @@
+
+//          Copyright Dominic Koepke 2017 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef GEORITHM_UTILITY_HPP
+#define GEORITHM_UTILITY_HPP
+
+#pragma once
+
+#include <cassert>
+#include <concepts>
+#include <type_traits>
+
+#include "Defines.hpp"
+
+namespace georithm
+{
+	template <class T>
+	[[nodiscard]] constexpr bool isNull(const T& object) noexcept
+	requires requires { {object.isNull() }->std::convertible_to<bool>; }
+	{
+		return object.isNull();
+	}
+
+	template <class T>
+	[[nodiscard]] constexpr VertexIndex_t vertexCount(const T& object) noexcept
+	requires requires { {object.vertexCount() }->std::convertible_to<VertexIndex_t>; }
+	{
+		return object.vertexCount();
+	}
+
+	template <class T>
+	[[nodiscard]] constexpr EdgeIndex_t edgeCount(const T& object) noexcept
+	requires requires { {object.edgeCount() }->std::convertible_to<EdgeIndex_t>; }
+	{
+		return object.edgeCount();
+	}
+
+	template <class T>
+	constexpr auto vertex(const T& object, VertexIndex_t index) noexcept -> decltype(object.vertex(index))
+	requires requires { { object.vertex(index) }->VectorObject; }
+	{
+		return object.vertex(index);
+	}
+
+	template <class T>
+	constexpr auto edge(const T& object, EdgeIndex_t index) noexcept -> decltype(object.edge(index))
+	requires requires { { object.edge(index) }->LineObject; }
+	{
+		return object.edge(index);
+	}
+}
+
+#endif
