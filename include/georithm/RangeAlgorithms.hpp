d342daddf70fcac3ef1ba2e8b9c820c740830548
DNKpp <DNKpp2011@gmail.com>
2020-05-06 21:27:43 +0200

    add RangeAlgorithms zip_elements
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

---
+++ include/georithm/RangeAlgorithms.hpp
@@ -0,0 +1,31 @@
+
+//          Copyright Dominic Koepke 2017 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef GEORITHM_RANGE_ALGORITHMS_HPP
+#define GEORITHM_RANGE_ALGORITHMS_HPP
+
+#pragma once
+
+#include <iterator>
+#include "Concepts.hpp"
+
+namespace georithm
+{
+	template <std::forward_iterator ForwardIt, std::sentinel_for<ForwardIt> ForwardItSentinel, std::input_iterator InputIt,
+		invocable_r<typename std::iterator_traits<ForwardIt>::value_type, typename std::iterator_traits<ForwardIt>::value_type, typename std::iterator_traits<InputIt>::value_type> BinaryOp>
+	constexpr void zip_elements(ForwardIt first, ForwardItSentinel last, InputIt secFirst, BinaryOp op)
+	{
+		while (first != last)
+		{
+			// ToDo: *first = std::invoke(op, *first, *secFirst);
+			*first = op(*first, *secFirst);
+			++first;
+			++secFirst;
+		}
+	}
+}
+
+#endif
