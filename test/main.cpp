a4e5c2a431b72589d8165c15f4d1e6b50153126a
DNKpp <DNKpp2011@gmail.com>
2020-08-05 23:54:48 +0200

    add custom main for test
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- test/main.cpp
+++ test/main.cpp
@@ -4,5 +4,15 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    int result = Catch::Session().run(argc, argv);
+
+    std::cin.get();
+    return result;
+}
