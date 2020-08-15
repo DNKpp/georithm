881d9c50edc457a131c3b6d1759c99e8fdfa0890
DNKpp <DNKpp2011@gmail.com>
2020-08-15 20:14:43 +0200

    add test cases for rotate
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- test/VectorTest.cpp
+++ test/VectorTest.cpp
@@ -1,11 +1,13 @@
-
 //          Copyright Dominic Koepke 2017 - 2020.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
 #include "catch.hpp"
-#include "../include/georithm/Vector.hpp"
+
+#include <numbers>
+
+#include "georithm/Vector.hpp"
 
 TEST_CASE("Vector constexpr compile test", "[Vector]")
 {
@@ -20,7 +22,9 @@ TEST_CASE("Vector constexpr compile test", "[Vector]")
 	};
 
 	constexpr auto vec2 = makeVector();
-	if constexpr (vec2[0] == 5) {};
+	if constexpr (vec2[0] == 5)
+	{
+	}
 }
 
 TEST_CASE("Vector operator test", "[Vector]")
@@ -64,19 +68,52 @@ TEST_CASE("Vector algorithm test", "[Vector]")
 	using namespace georithm;
 
 	Vector<int, 2> vec{ 1, 2 };
-	REQUIRE(lengthSq(vec) == (vec.x() * vec.x() + vec.y() * vec.y()));
-	REQUIRE(scalarProduct(vec, vec) == lengthSq(vec));
-	REQUIRE(length(vec) == 2);	// sqrt converted to int
-	REQUIRE(length<double>(vec) == Approx(std::sqrt((vec.x() * vec.x() + vec.y() * vec.y()))));
+	SECTION("length and normalize")
+	{
+		REQUIRE(lengthSq(vec) == (vec.x() * vec.x() + vec.y() * vec.y()));
+		REQUIRE(scalarProduct(vec, vec) == lengthSq(vec));
+		REQUIRE(length(vec) == 2); // sqrt converted to int
+		REQUIRE(length<double>(vec) == Approx(std::sqrt((vec.x() * vec.x() + vec.y() * vec.y()))));
 
-	auto vecf = static_cast<Vector<float, 2>>(vec * (-1));
-	REQUIRE(vecf.x() == Approx(-1));
-	REQUIRE(vecf.y() == Approx(-2));
-	REQUIRE(length(normalize(vecf)) == Approx(1));
+		auto vecf = static_cast<Vector<float, 2>>(vec * -1);
+		REQUIRE(vecf.x() == Approx(-1));
+		REQUIRE(vecf.y() == Approx(-2));
+		REQUIRE(length(normalize(vecf)) == Approx(1));
 
-	vecf = abs(vecf);
-	REQUIRE(vecf.x() == Approx(1));
-	REQUIRE(vecf.y() == Approx(2));
-	REQUIRE(length(normalize(vecf)) == Approx(1));
+		vecf = abs(vecf);
+		REQUIRE(vecf.x() == Approx(1));
+		REQUIRE(vecf.y() == Approx(2));
+		REQUIRE(length(normalize(vecf)) == Approx(1));
+	}
+
+	SECTION("rotate")
+	{
+		SECTION("pi")
+		{
+			vec = rotate(vec, std::numbers::pi);
+			REQUIRE(vec.x() == -1);
+			REQUIRE(vec.y() == -2);
+		}
+
+		SECTION("pi/2")
+		{
+			vec = rotate(vec, std::numbers::pi / 2);
+			REQUIRE(vec.x() == -2);
+			REQUIRE(vec.y() == 1);
+		}
+
+		SECTION("2*pi/3")
+		{
+			vec = rotate(vec, std::numbers::pi * 3 / 2);
+			REQUIRE(vec.x() == 2);
+			REQUIRE(vec.y() == -1);
+		}
+
+		SECTION("10*pi")
+		{
+			vec = rotate(vec, std::numbers::pi * 10);
+			REQUIRE(vec.x() == 1);
+			REQUIRE(vec.y() == 2);
+		}
+	}
 }
-
